ae6d2c9c91a693470b914a23a8bc3457a0400ae6
Quentin <Q.C.L.Lee@student.tudelft.nl>
2018-10-12 14:47:24 +0200

    javadoc will not be added again when it already exists

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,9 @@ fn generate_javadoc(path: &str) {
     let lines = output.lines();
     
     let mut res = String::from("");
-    for (index, line) in lines.enumerate() {
-        if line.contains("get") && line.contains("public") {
+    let mut has_javadoc = false;
+    for line in lines {
+        if !has_javadoc && line.contains("get") && line.contains("public") {
             let field_name = get_field_name(line);
             let description = format!("     * The getter for the {} field of this class\n", field_name);
             let return_text = format!("     * @return returns the value of the {} field of this class\n", field_name);
@@ -22,7 +23,7 @@ fn generate_javadoc(path: &str) {
             res.push_str(&return_text);
             res.push_str(&String::from("     */\n"));
         }
-        if line.contains("set") && line.contains("public"){
+        if !has_javadoc && line.contains("set") && line.contains("public"){
             let field_name = get_param_name(line).to_string();
             let description = format!("     * The setter for the {} field of this class\n", field_name);
             let param_description = format!("     * @param {} The new value assigned to the {} field of the object\n", field_name, field_name);
@@ -31,14 +32,18 @@ fn generate_javadoc(path: &str) {
             res.push_str(&param_description);
             res.push_str(&String::from("     */\n"));
         }
-
+        
         res.push_str(&line.to_string());        
         res.push_str("\n");
+        has_javadoc = false;
+        if line.contains("*/") {
+            has_javadoc = true;
+        }
     }
 
     println!("{}", res);
     
-    fs::write(path, res);
+    fs::write(path, res).expect("adding javadoc to file did not succeed");
 
 }
 
