4b7319c2d98eeedb6c2c67629c2f378806ec9911
Quentin Lee <Quentin.c.l.lee@gmail.com>
2018-10-30 16:08:01 +0100

    Add fix for != operator

--- src/checkstyle_fix_spaces.rs
+++ src/checkstyle_fix_spaces.rs
@@ -9,9 +9,9 @@ pub fn fix_spaces_all_files(files: &Vec<String>) {
 
 /// Fix the checkstyle errors which are caused by missing spaces.
 fn fix_spaces(content: String) -> String {
-    let re_before = Regex::new(r"(?P<before>[^/\-+*}{=\s])(?P<c>[/\-+*}{=])").unwrap();
+    let re_before = Regex::new(r"(?P<before>[^!/\-+*}{=\s])(?P<c>[/\-+*}{=])").unwrap();
     let temp_res = re_before.replace_all(&content[..], "$before $c");
-    let re_after = Regex::new(r"(?P<c>[/\-+*},{=])(?P<after>[^/\-+*}{=\s>])").unwrap();
+    let re_after = Regex::new(r"(?P<c>[/\-+*},{=])(?P<after>[^!/\-+*}{=\s>])").unwrap();
     let temp_content = String::from(temp_res);
     let result = re_after.replace_all(&temp_content[..], "$c $after"); 
     result.to_string()
@@ -48,6 +48,11 @@ mod tests {
 
     #[test]
     fn test_lambda() {
-        assert_eq!(fix_spaces(String::from("x->{  }")), String::from("x -> {  }"))
+        assert_eq!(fix_spaces(String::from("x->{  }")), String::from("x -> {  }"));
+    }
+
+    #[test]
+    fn test_not_equals_operator() {
+        assert_eq!(fix_spaces(String::from("!=")), String::from("!=")); 
     }
 }
